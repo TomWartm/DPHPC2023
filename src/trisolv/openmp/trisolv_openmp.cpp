311bdd9bb5de6cd60b94fabe0a6b46812ff17d7b
Johannes Gasser <johannes.gasser@adibilis.ch>
2023-11-14 10:11:11 +0100

    fix faulty implementation

--- src/trisolv/openmp/trisolv_openmp.cpp
+++ src/trisolv/openmp/trisolv_openmp.cpp
@@ -2,42 +2,36 @@
 #include <iostream>
 
 #define PAD 8
+
+// TODO: Test what number of threads is optimal
 #define NUM_THREADS 8
 
 void trisolv_openmp(int n, double* L, double* x, double* b)
 {
   for (int i = 0; i < n; i++)
   {
-      x[i] = b[i];
-      if(i > 10000) {
-          int sums[NUM_THREADS][PAD];
-          int block_size = i / NUM_THREADS;
-          omp_set_num_threads(NUM_THREADS);
-          #pragma omp parallel
-          {
-              int id = omp_get_thread_num();
-              int max;
-              if (id == NUM_THREADS - 1) {
-                  max = i;
-              } else {
-                  max = block_size * (id + 1);
-              }
-
-              //std::cout << printf("Iteration %d: Thread %d: - min: %d max: %d\n", i, id, start, max);
-              for (int j = id * block_size; j < max; j++) {
-                  sums[id][0] += -L[i * n + j] * x[j];
-              }
+      double sums[NUM_THREADS][PAD];
+      int block_size = i / NUM_THREADS;
+      omp_set_num_threads(NUM_THREADS);
+      #pragma omp parallel
+      {
+          int id = omp_get_thread_num();
+          int start = id * block_size;
+          int end;
+          if (id == NUM_THREADS - 1) {
+              end = i;
+          } else {
+              end = start + block_size;
           }
-          for(int j = 0; j < NUM_THREADS; j++) {
-              x[i] = x[i] + sums[j][0];
-          }
-      } else {
-          for (int j = 0; j < i; j++) {
-              double tmp = -L[i * n + j] * x[j];
-              x[i] = x[i] + tmp;
+          sums[id][0] = 0.0;
+          for (int j = start; j < end; j++) {
+              sums[id][0] += -L[i * n + j] * x[j];
           }
       }
-
+      x[i] = b[i];
+      for(int j = 0; j < NUM_THREADS; j++) {
+          x[i] += sums[j][0];
+      }
       x[i] = x[i] / L[i * n + i];
   }
 }
