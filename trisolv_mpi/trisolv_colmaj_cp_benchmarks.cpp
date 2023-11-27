b9802440967cfc0ce4c6f256ac7dc531818f64fa
Haoanqin Gao <haogao@ethz.ch>
2023-11-27 14:20:58 +0100

    fix output, now only print median

--- trisolv_mpi/trisolv_colmaj_cp_benchmarks.cpp
+++ trisolv_mpi/trisolv_colmaj_cp_benchmarks.cpp
@@ -5,6 +5,7 @@
 #include <omp.h>
 #include <chrono>
 #include <iomanip>
+#include <algorithm>
 
 void init(int N, double* A, double* x, double* b) {
 	for (int j = 0; j < N; ++j) {
@@ -20,84 +21,100 @@ void init(int N, double* A, double* x, double* b) {
 	}
 }
 
-void benchmark(int size, int rank) {
-	double *A, *x, *b;
-	for (int i = 6; i <= POW; ++i) {
-		int NDEF = std::pow(2, i);
-		int std_rows = std::ceil(1.0 * NDEF / size);
-        int rows;
-        if (rank == 0) rows = NDEF;
-        else rows = std_rows;
-        /****************INITIALIZATION******************/
-        if (rank == 0) {
-            A = (double*)malloc(NDEF * NDEF * sizeof(double));
-            x = (double*)malloc(NDEF * sizeof(double));
-            b = (double*)malloc(NDEF * sizeof(double));
-            init(NDEF, A, x, b);
-            for (int n = 1; n < size; ++n) {
-                for (int j = 0; j < NDEF; ++j) {
-                    MPI_Send(A + n * std_rows + j * NDEF, std_rows, MPI_DOUBLE, n, 1, MPI_COMM_WORLD);
-                }
-                std::cout << rank << " sending " << std_rows * NDEF << " elements to " << n << "\n";
-                MPI_Send(x, NDEF, MPI_DOUBLE, n, 1, MPI_COMM_WORLD);
-                MPI_Send(b, NDEF, MPI_DOUBLE, n, 1, MPI_COMM_WORLD);
-            }
-        }
-        else {
-            A = (double*)malloc(std_rows * NDEF * sizeof(double));
-            x = (double*)malloc(NDEF * sizeof(double));
-            b = (double*)malloc(NDEF * sizeof(double));
+double run (int size, int rank, int N) {
+    double time = 0;
+    double *A, *x, *b;
+    int NDEF = N;
+    int std_rows = std::ceil(1.0 * NDEF / size);
+    int rows;
+    if (rank == 0) rows = NDEF;
+    else rows = std_rows;
+    /****************INITIALIZATION******************/
+    if (rank == 0) {
+        A = (double*)malloc(NDEF * NDEF * sizeof(double));
+        x = (double*)malloc(NDEF * sizeof(double));
+        b = (double*)malloc(NDEF * sizeof(double));
+        init(NDEF, A, x, b);
+        for (int n = 1; n < size; ++n) {
             for (int j = 0; j < NDEF; ++j) {
-                MPI_Recv(A + j * std_rows, std_rows, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Send(A + n * std_rows + j * NDEF, std_rows, MPI_DOUBLE, n, 1, MPI_COMM_WORLD);
             }
-            std::cout << rank << " received " << std_rows * NDEF << " elements\n";
-            MPI_Recv(x, NDEF, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Recv(b, NDEF, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(x, NDEF, MPI_DOUBLE, n, 1, MPI_COMM_WORLD);
+            MPI_Send(b, NDEF, MPI_DOUBLE, n, 1, MPI_COMM_WORLD);
         }
-        /************************************************/
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        /****************START TIMER******************/
-        std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-        if (rank == 0) {
-            start = std::chrono::high_resolution_clock::now();
-        }
-        /*********************************************/
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        /****************COMPUTATION******************/
+    }
+    else {
+        A = (double*)malloc(std_rows * NDEF * sizeof(double));
+        x = (double*)malloc(NDEF * sizeof(double));
+        b = (double*)malloc(NDEF * sizeof(double));
         for (int j = 0; j < NDEF; ++j) {
-            int rank_x_std_rows = rank * std_rows;
-            int j_x_rows = j * rows;
-            x[j] = b[j] / A[(j - rank_x_std_rows) * rows + j];
-            MPI_Bcast(x + j, 1, MPI_DOUBLE, j / std_rows, MPI_COMM_WORLD);
-            for (int i = 0; i < std_rows; ++i) {
-                b[rank_x_std_rows + i] -= A[j_x_rows + i] * x[j];
-            }
+            MPI_Recv(A + j * std_rows, std_rows, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
-        /*********************************************/
+        MPI_Recv(x, NDEF, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(b, NDEF, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    /************************************************/
 
-        MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
 
-        /****************END TIMER******************/
-        if (rank == 0) {
-            end = std::chrono::high_resolution_clock::now();
+    /****************START TIMER******************/
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    if (rank == 0) {
+        start = std::chrono::high_resolution_clock::now();
+    }
+    /*********************************************/
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    /****************COMPUTATION******************/
+    for (int j = 0; j < NDEF; ++j) {
+        int rank_x_std_rows = rank * std_rows;
+        int j_x_rows = j * rows;
+        x[j] = b[j] / A[(j - rank_x_std_rows) * rows + j];
+        MPI_Bcast(x + j, 1, MPI_DOUBLE, j / std_rows, MPI_COMM_WORLD);
+        for (int i = 0; i < std_rows; ++i) {
+            b[rank_x_std_rows + i] -= A[j_x_rows + i] * x[j];
+        }
+    }
+    /*********************************************/
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    /****************END TIMER******************/
+    if (rank == 0) {
+        end = std::chrono::high_resolution_clock::now();
 #ifdef PRINTX
-            std::cout << "x = [";
+        std::cout << "x = [";
 		for (int i = 0; i < NDEF; ++i) std::cout << x[i] << " ";
 		std::cout << "]\n";
 #endif
-            const std::chrono::duration<double> diff = end - start;
-            std::cout << std::fixed << std::setprecision(9) << std::left;
-            std::cout << "Time: " << diff.count() << '\n';
-        }
-        /*******************************************/
+        const std::chrono::duration<double> diff = end - start;
+        //std::cout << std::fixed << std::setprecision(9) << std::left;
+        //std::cout << "Time: " << diff.count() << '\n';
+        time = diff.count();
+    }
+    /*******************************************/
 
-        free(A);
-        free(x);
-        free(b);
+    free(A);
+    free(x);
+    free(b);
+    return time;
+}
+
+void benchmark(int size, int rank) {
+	double *A, *x, *b;
+	for (int i = 6; i <= POW; ++i) {
+        int N = std::pow(2, i);
+        std::vector<double> time;
+        for (int j = 0; j < REPEAT; ++j) {
+            time.push_back(run(size, rank, N));
+        }
+        if (rank == 0) {
+            std::sort(time.begin(), time.end());
+            std::cout << N << "\t";
+            std::cout << std::fixed << std::setprecision(9) << std::left;
+            std::cout << time[REPEAT / 2] << '\n';
+        }
 	}
 }
 
