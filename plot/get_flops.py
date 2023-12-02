1b96c0b5cb0fc0fe223c45118bd9439cfe918061
Tom Wartmann <tomw@ethz.ch>
2023-12-02 11:19:38 +0100

    add performance plot to gemver (counting flops)

---
+++ plot/get_flops.py
@@ -0,0 +1,22 @@
+def get_flops_gemver(method:str, n:int) -> int:
+    """
+    Returns number of floating point operations (additions and multiplications) of the gemver baseline function.
+    Index operations are ignored.
+    """
+    no_additions = 0
+    no_multiplications = 0
+    if method in ["baseline","gemver_mpi_2", "gemver_mpi_3"]:
+        no_additions = n*n*2 + n*n + n + n*n
+        no_multiplications = n*n*2 + n*n*2 + n*n*2
+    
+    elif method in ["baseline blocked 1", "baseline blocked 2", "gemver_mpi_4"]:
+        n = n/4
+
+        no_additions = n*n* 32 + n*n* 16 + n*n* 16 # same as in baseline
+        no_multiplications = n*n*32 + n*n*20 + n*n*20 # less as baseline
+    else:
+        print("Not implemented flop count for method: {}".format(method))
+    
+    return no_additions + no_multiplications
+
+
