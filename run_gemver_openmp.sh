7969b1d915d7cac6ff70b851b043b7b028ee9413
igashi <igashi@student.ethz.ch>
2024-01-10 12:58:41 +0000

    Update run_gemver_openmp.sh

--- run_gemver_openmp.sh
+++ run_gemver_openmp.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 # Loop over the desired thread counts
-for thread in 1 2
+for thread in 1 2 4 8 16 32 48
 do
-    # Update the makefile to set the new NUM_THREADS value
-    sed -i "s/-DNUM_THREADS=[0-9]*/-DNUM_THREADS=$thread/" Makefile
-
-    # Recompile the program
-    make clean
-    make
-
-    # Submit the job with the appropriate number of CPUs
-    ./evaluate_gemver_openmp
+    export OMP_NUM_THREADS=$thread
+    sbatch --ntasks=1 --cpus-per-task=$thread --wrap="./evaluate_gemver_openmp"
 done
